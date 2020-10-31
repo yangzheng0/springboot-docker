bf925be1a9b3582c14d37cd8b136191510cd0e3e
yangzheng <yz6780610>
2020-10-31 18:24:30 +0800

    Initial commit

---
+++ src/main/java/com/springboot/docker/springbootdocker/controller/DockerController.java
@@ -0,0 +1,19 @@
+package com.springboot.docker.springbootdocker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangzheng
+ * @version 1.0
+ * @description
+ * @date 2020/10/31 031 13:54
+ */
+@RestController
+public class DockerController {
+
+    @RequestMapping("/docker")
+    public String test() {
+        return "docker";
+    }
+}
